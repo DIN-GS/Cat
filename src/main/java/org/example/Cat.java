384a72e1f1247a32f8d1acc9e1dbe2fc1bcd2251
DIN-GS <nikita.ivakin@nure.ua>
2022-11-11 16:32:08 +0200

    realisation

--- src/main/java/org/example/Cat.java
+++ src/main/java/org/example/Cat.java
@@ -3,5 +3,66 @@ package org.example;
 import java.util.Objects;
 
 public class Cat extends Predator{
+    private String breed;
 
+    public Cat(String colour, int weight, String breed) {
+        super(colour, weight);
+        this.breed = breed;
+    }
+
+    @Override
+    public String sleeping() {
+        System.out.println("Cat is sleeping");
+        return "Cat is sleeping";
+    }
+
+    @Override
+    public String hunting() {
+        System.out.println("Cat is hunting");
+        return "Cat is hunting";
+    }
+
+    public String jumping(){
+        System.out.println("TUguduktuguduk");
+        return "TUguduktuguduk";
+    }
+
+    public String whatIsCatDoing(Days day){
+        switch (day){
+            case Monday:
+            case Wednesday:
+            case Friday:
+                return sleeping();
+
+            case Tuesday:
+            case Thursday:
+            case Saturday:
+                return hunting();
+
+
+            case Sunday:
+                return jumping();
+
+            default:
+                System.out.println("Hmm I don't know where is my cat and what it is doing");
+                return "Hmm I don't know where is my cat and what it is doing";
+        }
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cat cat = (Cat) o;
+
+
+        return Objects.equals(breed, cat.breed)  && super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return breed != null ? breed.hashCode() : 0;
+    }
 }
--- src/test/java/org/example/CatTest.java
+++ src/test/java/org/example/CatTest.java
@@ -3,6 +3,19 @@ package org.example;
 import junit.framework.TestCase;
 
 public class CatTest extends TestCase {
+    public void testWhatIsCatDoing() {
+        Cat cat = new Cat("White", 2, "Британська короткошерста");
 
+        assertEquals("Cat is sleeping",cat.whatIsCatDoing(Days.Monday));
+
+    }
+
+    public void testTestEquals() {
+        Cat cat = new Cat("White", 2, "Британська короткошерста");
+        Cat cat2 = new Cat("White", 2, "Британська короткошерста");
+
+        assertEquals(true, cat.equals(cat2));
+
+    }
 
 }
